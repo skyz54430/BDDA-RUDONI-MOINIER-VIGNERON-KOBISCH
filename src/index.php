bd4b14a85388840567b3259fe91da2e78067b178
RUDONI Antonin <antonin.rudoni3@etu.univ-lorraine.fr>
2022-03-14 14:18:13 +0100

    Update index.php

--- src/index.php
+++ src/index.php
@@ -46,3 +46,106 @@ try {
 }catch (Throwable $e) {
     print_r("\t ERREUR \n");
 }
+
+
+
+//Q1 : • afficher (name , deck) les personnages du jeu 12342
+print_r("Q1 : \n\n");
+$jeu = Game::find(12342);
+foreach ($jeu->hasCharacters as $c){
+    echo $c->name . ", " . $c->deck . " \n";
+}
+
+//Q2 : • les personnages des jeux dont le nom (du jeu) débute par 'Mario'
+print_r("Q2 : \n\n");
+$jeux = Game::where('name', 'like', '%mario%')->get();
+foreach ($jeux as $j){
+    echo $j->name . " : \n";
+    foreach ($j->hasCharacters as $c){
+        echo $c->id . ", "  . $c->name . " \n";
+    }
+}
+
+//Q3 : • les jeux développés par une compagnie dont le nom contient 'Sony'
+print_r("Q3 : \n\n");
+$jeux = Game::whereHas('compagniesDevelopers', function ($q){
+    $q->where('name', 'like', '%sony%');})->get();
+
+
+foreach ($jeux as $j) {
+
+    echo $j->id . ", " . $j->name . "\n";
+
+}
+
+//Q4 : • le rating initial (indiquer le rating board) des jeux dont le nom contient Mario
+print_r("Q4 : \n\n");
+$ratings = Game_rating::whereHas('original_game_ratings', function ($q) {
+    $q->where('name', 'like', '%mario%');})->get();
+
+foreach ($ratings as $r) {
+    $ratingBoard = $r->game_ratingTorating_board;
+    echo $ratingBoard-> id . " ," . $ratingBoard->name . " , " . $r->id . ", " . $r->name . "\n";
+
+}
+
+//Q5 : • les jeux dont le nom débute par Mario et ayant plus de 3 personnages
+print_r("Q5 : \n\n");
+$jeux = Game::where('name', 'like', '%mario%')->get();
+foreach ($jeux as $j){
+    $countChar = $j->hasCharacters()->count();
+    if ($countChar > 3) {
+        echo $j->id . ", " . $j->name . "\n";
+    }
+}
+
+//Q6 : • les jeux dont le nom débute par Mario et dont le rating initial contient "3+"
+print_r("Q6 : \n\n");
+$jeux = Game::where('name', 'like', 'Mario%')->whereHas('gameRating', function($q){
+    $q->where('name','like','%3+%');
+})->get();
+foreach ($jeux as $j){
+    echo $j->name . "\n";
+}
+
+//Q7 : • les jeux dont le nom débute par Mario, publiés par une compagnie dont le nom contient "Inc." et dont le rating initial contient "3+"
+print_r("Q7 : \n\n");
+$jeuQ7 = Game::where('name', 'like', 'Mario%')->whereHas('compagniesPublies', function($query) {
+    $query->where('name', 'like', '%Inc.%');
+    })->whereHas('gameRating', function($q){
+    $q->where('name','like','%3+%');
+})->get();
+foreach ($jeuQ7 as $j7){
+    echo $j7->name . "\n";
+}
+
+
+//Q8 : • les jeux dont le nom débute Mario, publiés par une compagnie dont le nom contient "Inc", dont le rating initial contient "3+" et ayant reçu un avis de la part du rating board nommé"CERO"
+print_r("Q8 : \n\n");
+$jeuQ8 = Game::where('name', 'like', 'Mario%')->whereHas('compagniesPublies', function($query) {
+    $query->where('name', 'like', '%Inc.%');
+})->whereHas('gameRating', function($q){
+    $q->where('name','like','%3+%');
+})->whereHas('gameRating.game_ratingTorating_board', function($qu){
+    $qu->where('name', 'like', 'CERO');
+})->get();
+foreach ($jeuQ8 as $j8){
+    echo $j8->name . "\n";
+}
+
+//Q9 : • ajouter un nouveau genre de jeu, et l'associer aux jeux 12, 56, 12, 345
+print_r("Q9 : \n\n");
+$nvGenre =new Genre();
+
+$nvGenre->id = Genre::all()->count() + 1;
+$nvGenre->name = "Genre perso";
+$nvGenre->description = "Nouveau genre cree specialement pas moi eheh";
+
+$jeu1 = Game::find(12);
+$jeu1->genres()->save($nvGenre);
+
+$jeu2 = Game::find(56);
+$jeu1->genres()->save($nvGenre);
+
+$jeu2 = Game::find(345);
+$jeu1->genres()->save($nvGenre);
